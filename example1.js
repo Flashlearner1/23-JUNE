986c6e534fb66e9543200eccf1ce7f860c8a113c
Flashlearner1 <osayandekingsley768@gmail.com>
2023-06-23 08:49:00 -0700

    Addition in Java

---
+++ example1.js
@@ -0,0 +1,20 @@
+const prompt = require("prompt-sync")({sigint: true});
+
+//function greet () {
+//    console.log("Good morning class")
+//}
+
+//greet();
+
+
+function add(x, y) {
+    return(x + y);
+}
+
+    let number1= 5
+    let number2 = 2
+
+    let result = add(number1, number2);
+
+    console.log(result)
+
